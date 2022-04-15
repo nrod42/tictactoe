b425cd3f760d8ba460bd1f5280632e14968b5863
nrod42 <nigelrod94@gmail.com>
2022-04-15 12:45:00 -0400

    tried to reformat using modules

--- script.js
+++ script.js
@@ -23,25 +23,6 @@ const winningCond = [
     [2, 4, 6]
 ];
 
-let player1;
-let player2;
-let currentPlayer;
-let currentBoard = [];
-let turn = 0;
-
-newGameForm.addEventListener('submit', showGame)
-
-newGameBtns.forEach(button => {
-    button.addEventListener('click', newGame);
-});
-
-cells.forEach(cell => {
-    cell.textContent = "";
-    cell.addEventListener('click', makeMove);
-})
-
-playAgainBtn.addEventListener('click', playAgain);
-
 const Player = (name, mark) => {
     const getName = () => name;
     const getMark = () => mark;
@@ -53,101 +34,125 @@ const Player = (name, mark) => {
     return {getName, getMark, getScore, addScore}
 };
 
-function showGame (e) {
-    e.preventDefault();
-    gameBoard.style.display = 'grid'
-    newGameForm.style.display = 'none'
-    player1 = Player(newGameForm.player1.value, 'X');
-    player2 = Player(newGameForm.player2.value, 'O');
-    currentPlayer = player1;
-    clickEnable();
-}
+const gameController = (() => {
 
-function makeMove(e) {
-    if (!e.target.classList.contains('clickEnabled')) return; //checks if clickEnabled class is active. If its not (which occurs when game is over and on result screen), then adding a mark is "disabled"
-    if (e.target.textContent != "") return; //prevents cells fromb being marked twice
-    e.target.textContent = currentPlayer.getMark();
-    currentBoard = cellsArray.map(cell => cell.textContent); //updates the current board array every turn
-    turn++;
-    checkTie();
-    showWinner();
-    switchPlayer();
-}
+    let player1;
+    let player2;
+    let currentPlayer;
+    let currentBoard = [];
+    let turn = 0;
 
-function switchPlayer() {
-    currentPlayer == player1 ? currentPlayer = player2 : currentPlayer = player1;
-}
+    const showGame = (e)  => {
+        e.preventDefault();
+        gameBoard.style.display = 'grid'
+        newGameForm.style.display = 'none'
+        player1 = Player(newGameForm.player1.value, 'X');
+        player2 = Player(newGameForm.player2.value, 'O');
+        currentPlayer = player1;
+        clickEnable();
+    }
 
-function showWinner () {
-    if (checkWin() == true) {
-        clickDisable();
-        winner(currentPlayer);
-    };
-}
+    const makeMove = (e) => {
+        if (!e.target.classList.contains('clickEnabled')) return; //checks if clickEnabled class is active. If its not (which occurs when game is over and on result screen), then adding a mark is "disabled"
+        if (e.target.textContent != "") return; //prevents cells fromb being marked twice
+        e.target.textContent = currentPlayer.getMark();
+        currentBoard = cellsArray.map(cell => cell.textContent); //updates the current board array every turn
+        turn++;
+        checkTie();
+        showWinner();
+        switchPlayer();
+    }
 
-function checkWin () {
-    for (let i = 0; i <= winningCond.length - 1; i++) {
-        let winCond = winningCond[i];
-        let a = currentBoard[winCond[0]];
-        let b = currentBoard[winCond[1]];
-        let c = currentBoard[winCond[2]];
-        if (a == "" || b == "" || c == "") continue;
-        if (a == b && b == c) {
-            return true;
+    const switchPlayer = () => {
+        currentPlayer == player1 ? currentPlayer = player2 : currentPlayer = player1;
+    }
+
+    const showWinner = () => {
+        if (checkWin() == true) {
+            clickDisable();
+            winner(currentPlayer);
+        };
+    }
+
+    const checkWin  = () => {
+        for (let i = 0; i <= winningCond.length - 1; i++) {
+            let winCond = winningCond[i];
+            let a = currentBoard[winCond[0]];
+            let b = currentBoard[winCond[1]];
+            let c = currentBoard[winCond[2]];
+            if (a == "" || b == "" || c == "") continue;
+            if (a == b && b == c) {
+                return true;
+            }
         }
     }
-}
 
-function winner (winner) {
-    resultScreen.style.display = 'flex';
-    gameScreen.style.filter = 'blur(2px)';
-    winnerDisplay.textContent  = winner.getName() + ' wins!';
-    winner == player1 ? player1.addScore(1) : player2.addScore(1);
-    player1Score.textContent = player1.getName() + ': ' + player1.getScore();
-    player2Score.textContent = player2.getName() + ': ' + player2.getScore();
-}
-
-function checkTie () {
-    if (turn >= 9) {
+    const winner  = (winner) => {
         resultScreen.style.display = 'flex';
         gameScreen.style.filter = 'blur(2px)';
-        winnerDisplay.textContent  = 'It\'s a tie!';
-    } 
-}
+        winnerDisplay.textContent  = winner.getName() + ' wins!';
+        winner == player1 ? player1.addScore(1) : player2.addScore(1);
+        player1Score.textContent = player1.getName() + ': ' + player1.getScore();
+        player2Score.textContent = player2.getName() + ': ' + player2.getScore();
+    }
 
-function clickEnable () {
-    cells.forEach(cell => {
-        cell.classList.add('clickEnabled');
-    });
-}
+    const checkTie = () => {
+        if (turn >= 9) {
+            resultScreen.style.display = 'flex';
+            gameScreen.style.filter = 'blur(2px)';
+            winnerDisplay.textContent  = 'It\'s a tie!';
+        } 
+    }
 
-function clickDisable () {
-    cells.forEach(cell => {
-        cell.classList.remove('clickEnabled');
-    });
-}
+    const clickEnable = () => {
+        cells.forEach(cell => {
+            cell.classList.add('clickEnabled');
+        });
+    }
 
-function newGame () {
-    currentPlayer = player1;
-    cells.forEach(cell => {
-        cell.textContent = "";
-        clickEnable(); //Re-adds the class which allows a mark to be placed again since its a new game
-    });
-    gameBoard.style.display = 'none';
-    newGameForm.style.display = 'flex';
-    newGameForm.player1.value = "";
-    newGameForm.player2.value = "";
-    resultScreen.style.display = 'none';
-    gameScreen.style.filter = 'blur(0)';
-    turn = 0;
-}
+    const clickDisable = () => {
+        cells.forEach(cell => {
+            cell.classList.remove('clickEnabled');
+        });
+    }
 
-function playAgain () {
-    resultScreen.style.display = 'none';
-    cells.forEach(cell => {
-        cell.textContent = "";
-        clickEnable();//Re-adds the class which allows a mark to be placed again since its a new game
-    });
-    gameScreen.style.filter = 'blur(0)';
-    turn = 0;
-}
+    const newGame = () => {
+        currentPlayer = player1;
+        cells.forEach(cell => {
+            cell.textContent = "";
+            clickEnable(); //Re-adds the class which allows a mark to be placed again since its a new game
+        });
+        gameBoard.style.display = 'none';
+        newGameForm.style.display = 'flex';
+        newGameForm.player1.value = "";
+        newGameForm.player2.value = "";
+        resultScreen.style.display = 'none';
+        gameScreen.style.filter = 'blur(0)';
+        turn = 0;
+    }
+
+    const playAgain = () => {
+        resultScreen.style.display = 'none';
+        cells.forEach(cell => {
+            cell.textContent = "";
+            clickEnable();//Re-adds the class which allows a mark to be placed again since its a new game
+        });
+        gameScreen.style.filter = 'blur(0)';
+        turn = 0;
+    }
+
+    return {showGame, makeMove, showWinner, newGame, playAgain};
+})();
+
+newGameForm.addEventListener('submit', gameController.showGame)
+
+newGameBtns.forEach(button => {
+    button.addEventListener('click', gameController.newGame);
+});
+
+cells.forEach(cell => {
+    cell.textContent = "";
+    cell.addEventListener('click', gameController.makeMove);
+})
+
+playAgainBtn.addEventListener('click', gameController.playAgain);
